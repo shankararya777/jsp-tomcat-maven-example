a85b53a6b880e786b1aa8ac36dc1098678ec9821
Decker108 <1337decker@gmail.com>
2023-12-26 22:25:59 +0100

    Switch from Jakarta to Javax

--- src/main/java/org/example/LoginServlet.java
+++ src/main/java/org/example/LoginServlet.java
@@ -2,11 +2,11 @@ package org.example;
 
 import java.io.IOException;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class for LoginServlet as defined in web.xml
